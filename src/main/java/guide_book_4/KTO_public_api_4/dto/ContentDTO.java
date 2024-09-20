7f9d58094515ce4911405c15fed37625015debdf
jmsparrowk <jmsparrowk@naver.com>
2024-09-20 21:45:57 +0900

    9/20

---
+++ src/main/java/guide_book_4/KTO_public_api_4/dto/ContentDTO.java
@@ -0,0 +1,12 @@
+package guide_book_4.KTO_public_api_4.dto;
+
+public class ContentDTO {
+    "contentid": "3097128",
+            "title": "가보정 4관",
+            "firstimage": "",
+            "firstimage2": "",
+            "areacode": "31",
+            "addr1": "경기도 용인시 수지구 수풍로 103"
+}
+
+}
